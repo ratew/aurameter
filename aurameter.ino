8530b9d2af5226da5cbc5d4ebd5178596cddbfdb
Emil <stiiviboi@outlook.com>
2025-06-07 00:15:21 +0300

    Update aurameter.ino
    added comments, updated username

--- aurameter.ino
+++ aurameter.ino
@@ -1,5 +1,5 @@
 /*
-This is the aura-meter sketch by Emil Hannes (ratew on github)
+This is the aura-meter sketch by Emil Hannes (emilhannes on github)
 So you have a LCD I2C, Sonar distance sensor, capacitive touch sensor and passive buzzer connected to the arduino uno r3
 first you go into default
 mode where the max aura is 3,000-10,000, if you hold the capacitive touch sensor then it goes to high mode
@@ -7,11 +7,14 @@ with a maximum aura of 1,000,000 - "fuse broken" (1,000,001)
 The buzzer buzzes accordingly aswell.
 */
 
+// include necessary libraries
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
+// create lcd object
 LiquidCrystal_I2C lcd(0x20,16,2); 
 
+// assign all variables
 const int buz = 2;
 const int cts = 3;
 const int trig_pin = 9;
@@ -22,6 +25,7 @@ int outputValue;
 float distanceB = 0.0;
 int outputValueB;
 
+// define all pins, initiate lcd and set pins 9 and 10 to low
 void setup() {
   pinMode(echo_pin, INPUT);
   pinMode(trig_pin, OUTPUT);
@@ -31,9 +35,11 @@ void setup() {
   digitalWrite(buz, LOW);
   lcd.backlight();
   lcd.init();
+  startsound(); // play starting sound
 }
 
 void loop() {
+  // get ultrasonic reading
   digitalWrite(trig_pin, LOW);
   delayMicroseconds(2);
   digitalWrite(trig_pin, HIGH);
@@ -46,48 +52,54 @@ void loop() {
   // Convert to distance in cm
   distance = duration * 0.034 / 2;
 
+  // print normal readinga
 if(digitalRead(cts) == LOW){
-   if (distance < 15){
+   if (distance < 15){ // if you are under 15 cm close to someone
     int maxOutput = random(3000, 10000);
-     outputValue = map(distance, 100, 2, 0, maxOutput);
+     outputValue = map(distance, 100, 2, 0, maxOutput); // map and constarin values
      outputValue = constrain(outputValue, 0, maxOutput);
+     // print everything
    lcd.setCursor(0, 0);
    lcd.print("Aura Level:   ");
    lcd.setCursor(0, 1);
    lcd.print(outputValue);
    lcd.setCursor(4, 1);
    lcd.print("            ");
+     //beep
    int beepsB = outputValue / 100;
    tone(buz, 1000);
    delay(beepsB);
    noTone(buz);
    }
  }
-else{
+else{ // if pressed
+
+  // print everything
    lcd.clear();
    lcd.setCursor(0,0);
    lcd.print("Aura Level:   ");
    lcd.setCursor(0, 1);
    lcd.print("9999999999999999");
-   for (int i=100; i>1; i--){
+   for (int i=100; i>1; i--){ // beep alot
      tone(buz, 3000);
      delay(i);
      noTone(buz);
     }
+  // print more to lcd
     lcd.setCursor(0, 1);
     lcd.print("FUSE FAILURE    ");
     delay(1000);
-    for(int x=1; x<6; x++){
+    for(int x=1; x<6; x++){ // beep more
       lcd.setCursor(15, 1);
       lcd.print(x);
       delay(600);
     }
-    delay(2);
+    delay(2); // wait 2 milliseconds
    }
  }
 
 
-void startsound(){
+void startsound(){ // start sound function
 
 tone(buz, 500);
 delay(10);
